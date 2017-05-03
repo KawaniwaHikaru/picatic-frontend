82b244701f3b3b0888121707661d9eea1cabc0a4
Brian Ho <brian@vandrico.com>
2017-05-03 12:46:45 -0700

    init commit of all files after scaffolding with yoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name picaticFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the picaticFrontendApp
+ */
+angular.module('picaticFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name picaticFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the picaticFrontendApp
+ */
+angular.module('picaticFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
